a48cdb2f6da35fd2dcaec895eb508286f736526a
jin <45493834+apollo2151994@users.noreply.github.com>
2019-02-02 11:55:47 +0800

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -6,5 +6,9 @@ app.post('/webhook/orders/create', (req, res) => {
     console.log("we got an order!");
     res.sendStatus(200);
 });
-
+app.get("/", (req, res)=>{
+    console.log("index.html");
+    res.send("my first project, cheer up");
+    // res.sendStatus(200);
+})
 app.listen(3000, ()=>console.log("Example app listening on port 3000!"));
